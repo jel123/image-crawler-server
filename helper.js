674844b3c3c60455651bedb229882ee3099f71bc
jquinamot <jquinamot@binaryanvil.com>
2023-07-06 00:26:09 +0800

    Fixed: buffer issues

--- helper.js
+++ helper.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 const path = require("path");
 const url = require("url");
-const probe = require("probe-image-size");
+const http = require("http");
+const https = require("https");
+const sizeOf = require("image-size");
+//const probe = require("probe-image-size");
 
 module.exports = {
   getFileSize: async (url) => {
@@ -21,14 +24,33 @@ module.exports = {
     const extension = path.extname(url);
     return extension.substring(1);
   },
-  getFileDimension: async (urlParam) => {
+  getFileDimension: (urlParam) => {
     const options = url.parse(urlParam);
     if ((options.protocol === "https:") | (options.protocol === "http:")) {
-      let result = await probe(urlParam, { rejectUnauthorized: false });
-      return {
-        width: result.width,
-        height: result.height,
-      };
+      // let result = await probe(urlParam);
+      // return {
+      //   width: result.width,
+      //   height: result.height,
+      // };
+
+      const client = options.protocol === "https:" ? https : http;
+
+      return new Promise((resolve, reject) => {
+        client.get(options, function (response) {
+          const chunks = [];
+          response
+            .on("data", function (chunk) {
+              chunks.push(chunk);
+            })
+            .on("end", function () {
+              const buffer = Buffer.concat(chunks);
+              resolve(sizeOf(buffer));
+            })
+            .on("error", (error) => {
+              reject(error);
+            });
+        });
+      });
     }
   },
 };
