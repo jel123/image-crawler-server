38357d57250eb0b30285668a60ba1bfc1591f01c
jquinamot <jquinamot@binaryanvil.com>
2023-07-06 00:31:30 +0800

    Fixed: buffer issues

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get("/api/data/:url", async (req, res) => {
     const request = await axios.request({
       method: "get",
       url: url,
-      timeout: 60000, //optional
+      timeout: 20000, //optional
       httpsAgent: new https.Agent({ keepAlive: true }),
       headers: header,
     });
